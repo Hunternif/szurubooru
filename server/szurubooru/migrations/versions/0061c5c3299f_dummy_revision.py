2a06a2475d54bd960d00c42dcea5acd3b593b69e
Hunternif <hunternif@gmail.com>
2022-02-14 01:32:43 +0000

    attempt to fix mismatch alembic revisions by creating a new dummy revision

---
+++ server/szurubooru/migrations/versions/0061c5c3299f_dummy_revision.py
@@ -0,0 +1,18 @@
+"""
+This is a dummy revision, trying to run master Szuru on my existing DB from hunternif
+
+Revision ID: 0061c5c3299f
+Created at: 2022-02-14 01:30:36.255429
+"""
+
+import sqlalchemy as sa
+from alembic import op
+
+revision = "0061c5c3299f"
+down_revision = "adcd63ff76a2"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
